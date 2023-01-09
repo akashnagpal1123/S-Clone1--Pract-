37aedae013652b81f649e456f53d266f804f5f08
Akash Nagpal <akashnagpalnov@gmail.com>
2023-01-09 18:42:51 +0530

    committing for config.cc file

--- src/login/log.js
+++ src/login/log.js
@@ -1,7 +1,9 @@
 window.setItemsInLocalStorage = ({ accessToken, tokenType, expiresIn }) => {
     localStorage.setItem(ACCESS_TOKEN, accessToken);
     localStorage.setItem(TOKEN_TYPE, tokenType);
-    localStorage.setItem(EXPIRES_IN, (Date.now() + (expiresIn * 1000)));
+
+    localStorage.setItem(EXPIRES_IN, 
+        (Date.now() + (expiresIn * 1000)));
     window.location.href = APP_URL;
 
 }
@@ -21,13 +23,17 @@ window.addEventListener("load", () => {
         }
 
         const { hash } = window.location;
+
         const searchParams = new URLSearchParams(hash);
+
         const accessToken = searchParams.get("#access_token");
+
 
         // #access_token = BQCpXqx_mZoHhCEIoAGbjxJqIG - QSlTm6zyM_zN6dqZhpvXxYGfF7ZKeLQd9dM3NX5QvJmQ1ovlOVGrsAw_vrURimONPMfRhn2J - mZXWlhnNVTZAcxvULe3YMNZZGhmr - PaZVpfBYDUbnsfQ9KFiFzFttfElKp - xwchWrbkoB8rf6_ggOLntd6jo0iUVMgz_FgFWAFbKffJHmspKCUzWS03NrTEdsXlKTBBiIQ & token_type=Bearer & expires_in=3600
 
         const tokenType = searchParams.get("token_type");
         const expiresIn = searchParams.get("expires_in");
+
         if (accessToken) {
             window.close();
             window.opener.setItemsInLocalStorage({ accessToken, tokenType, expiresIn });
